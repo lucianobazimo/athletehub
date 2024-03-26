6febcaa6afc342fb0d0a7882e6d22e4a498c2906
Luciano Griffith <luciano.griffith@bazimo.fr>
2024-03-26 23:15:49 +0100

    feat(backend): change elysia to adonis

--- backend/docker/setup-db.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER elysia with encrypted password 'elysia';
-CREATE DATABASE elysia_app;
-ALTER DATABASE elysia_app OWNER TO elysia;
-
-GRANT ALL ON SCHEMA public TO elysia;
-GRANT ALL PRIVILEGES ON DATABASE elysia_app TO elysia;
